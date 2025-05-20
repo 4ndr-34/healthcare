246eb755599596ae79670afcf4ca25d4b6c02a51
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2025-05-21 01:31:51 +0200

    repos and some queries needed for creating an appointment request

--- src/main/java/com/example/healthcare/repository/StaffRepository.java
+++ src/main/java/com/example/healthcare/repository/StaffRepository.java
@@ -4,16 +4,19 @@ import com.example.healthcare.entity.MedicalStaff;
 import com.example.healthcare.model.register.RegisterUserRequestDTO;
 import com.example.healthcare.model.register.SuccessfulRegisterDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StaffRepository extends JpaRepository<MedicalStaff, Long> {
 
-    SuccessfulRegisterDTO save(RegisterUserRequestDTO registerUserRequestDTO);
+    Optional<MedicalStaff> findByFirstNameAndLastName(String firstName, String lastName);
 
-
+    @Query(value = "SELECT * FROM staff WHERE department=:departmentName AND first_name=:firstName AND last_name=:lastName LIMIT 1", nativeQuery = true)
+    Optional<MedicalStaff> findByDepartmentAndFirstNameAndLastName(String departmentName, String firstName, String lastName);
 
 
 }
