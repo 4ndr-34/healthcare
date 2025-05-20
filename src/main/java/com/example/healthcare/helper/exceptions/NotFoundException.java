5bb5905ae9c1979e51c8eaa370f7489911d94717
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2025-05-21 01:30:14 +0200

    added error messages to exceptions

--- src/main/java/com/example/healthcare/helper/exceptions/NotFoundException.java
+++ src/main/java/com/example/healthcare/helper/exceptions/NotFoundException.java
@@ -1,4 +1,9 @@
 package com.example.healthcare.helper.exceptions;
 
 public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
 }
